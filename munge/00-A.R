bf46f636000448e88730f512d82cd5918ae1a7ee
UTexas80 <glen.falk@utexas.edu>
2019-01-09 15:05:51 -0500

    20190109AccessFilesA
    a. added files to enable the code to run in Visual Studio 2017.
    b. added script to concatenate name from four columns into one column.

--- munge/00-A.R
+++ munge/00-A.R
@@ -1,9 +1,5 @@
+# how-to-import-most-recent-csv-file-into-rstudio
+# https://tinyurl.com/ydgy9pfs
 
- rm(list = ls()[grepl("(SQL|X2016Tuition)", ls())])    
-# rm(list = ls()[grepl("(SQL|Sheet|Export|X2016Tuition)", ls())])    
-
-#dataMeta package processing
-# variable_type <- c(1, 1)
-# variable_description <- c("DETAIL_CODE_DESC", "DEGREE.MAJOR")
-# linker <- build_linker(my.data = jctTbl.source, variable_description = variable_description, variable_type = variable_type)
-# metadata <- build_dict(my.data = jctTbl.source, linker = linker, option_description = NULL, prompt_varopts = FALSE)
+data_files <- file.info(Sys.glob("O:/IT/Access Files/*.csv"))                                                                     
+access_file_old <-read.csv(row.names(data_files)[which.max(data_files[["ctime"]])])   
--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,5 @@
-# Example preprocessing script.
+new <- 
+  new %>%
+  mutate(Name_Suffix2 = gsub("#NA", "", Name_Suffix2)) %>%
+  unite(surname, Last_Name2, First_Name2, sep = ", ") %>%
+  unite(employee_name, surname, Middle_Name2, Name_Suffix2, sep = " ")
